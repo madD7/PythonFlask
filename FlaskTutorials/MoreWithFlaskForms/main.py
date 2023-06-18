db381ac9e997d3916d724ddb669664b4b9d47d1c
madD7 <sdhaval.3003@gmail.com>
2023-06-18 18:39:53 +0530

    Using String, submit, select, TextArea, Boolean, Radio fields .

--- FlaskTutorials/MoreWithFlaskForms/main.py
+++ FlaskTutorials/MoreWithFlaskForms/main.py
@@ -1,6 +1,18 @@
-from flask import Flask, render_template
-from flask_wtf import FlaskForm             # Inherit class to create forms
-from wtforms import StringField, SubmitField
+from flask import (Flask, render_template,
+                   session, redirect, url_for)
+from flask_wtf import FlaskForm  # Inherit class to create forms
+
+
+
+"""
+Almost every HTML form field has a corresponding wtforms class.
+wtforms also has validators. 
+Validators can perform check on the form data,
+    eg: requiring a field to be filled.
+"""
+from wtforms import (StringField, SubmitField, SelectField, TextAreaField,
+                     BooleanField, DateTimeField, RadioField)
+from wtforms.validators import DataRequired
 
 """
 Using flask_wtf library and  wtforms package to create forms from python script.
@@ -14,16 +26,16 @@ Using flask_wtf library and  wtforms package to create forms from python script.
     Handle Form submission
 
 In this example, we will create form class and view functions in same script.
+
+Using Flask's session object to grab info provided 
+in the form and pass it to another template.
 """
 
 app = Flask(__name__)
 
-# Configure a secret key for csrf security
-# csrf = Cross site request forgery.
-
+# Configure a secret key for csrf (Cross site request forgery) security
 # Any string can be configured as secret key in config dict.
-# We will later learn better ways to configure secret key.
-# We can set-up secret key as an environment variable so that the key is not readable.
+# We can set up secret key as an environment variable so that the key is not readable.
 app.config['SECRET_KEY'] = 'mySecretKey'
 
 
@@ -32,7 +44,25 @@ class HomeForm(FlaskForm):
     My sample Form class, inherits FlaskForm class.
     """
     # attributes that will be sent to template
-    pup_breed = StringField("What breed is the pup?")     # put string for label
+    pup_breed = StringField("What breed is the pup?",  # string for label
+                            validators=[DataRequired()])  # pass new instance of DataRequired.
+    # Validator=DataRequired() means, this field must be compulsorily filled to submit the form
+
+    vaccinated = BooleanField("Is the pup vaccinated?")
+    mood = RadioField("Choose pup's mood:",
+                      choices=[('mood_one', 'Happy'),
+                               ('mood_two', 'Playful'),
+                               ('mood_three', 'Calm')])
+    # choices is a list of tupple pairs => value,label
+    # value - seen in backend, label - seen by user on frontend
+    # Check values displayed on the 'thankyou' page.
+
+    food_choice = SelectField("Select pup's fav food: ",
+                              choices=[('chi', 'Chicken'),
+                                       ('pdg', 'Pedigree'),
+                                       ('dgb', 'Dog Biscuits')])
+
+    feedback = TextAreaField()
     submit = SubmitField("Submit")
 
 
@@ -47,14 +77,31 @@ def home():
     # Check if form is valid on submission
     if form.validate_on_submit():
         # if valid, grab data
-        breed = form.pup_breed.data
-        form.pup_breed.data = ''
+        """
+            a session is just a time interval from when a client
+            logs in upto until when client logs out
+            As the session is imported, we have the session live for
+            that particular user and hence DON'T need to pass it.
+            session can be used in html.
+        """
+        session['breed'] = form.pup_breed.data
+        session['vaccinated'] = form.vaccinated.data
+        session['mood'] = form.mood.data
+        session['food'] = form.food_choice.data
+        session['feedback'] = form.feedback.data
 
-    return render_template('home.html',
-                           form=form, breed=breed)
+        # when form is filled, redirect to 'thankyou' view.
+        return redirect(url_for('thankyou'))
+
+    # if form is not filled, render home.html page.
+    return render_template('home.html', form=form)
 # End of home()
+
+
+@app.route('/thankyou')
+def thankyou():
+    return render_template('thankyou.html')
 
 
 if __name__ == "__main__":
     app.run(debug=True)
-
