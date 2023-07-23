5a31911fc8b9f15b3d87f8e0b1dd5a9d1fc9d499
madD7 <sdhaval.3003@gmail.com>
2023-07-23 22:24:27 +0530

    Directory structure created.

---
+++ FlaskTutorials/CompanyBlog/app.py
@@ -0,0 +1,4 @@
+from puppyCompanyBlog import app
+
+if __name__ == "__main__":
+    app.run(debug=True)
---
+++ FlaskTutorials/CompanyBlog/puppyCompanyBlog/__init__.py
@@ -0,0 +1,6 @@
+# This __init__ file will hold organisational logic - connecting blueprints,
+# connecting login manager & connecting everything together.
+# This will help organise.
+from flask import Flask
+
+app = Flask(__name__)
